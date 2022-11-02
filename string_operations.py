9d35ae6b2f55527f43d7e228724960b4b1b1d372
Ilya Smut <ilya.smut.off.g@gmail.com>
2022-11-02 20:38:50 +0000

    added string operations

---
+++ string_operations.py
@@ -0,0 +1,8 @@
+#You can add to strings together
+print('I love ' + 'you')
+#output: I love you
+
+#It is possible to split strings into a list using special characters
+print('My name is:Ilya'.split(':'))
+#Output: ['My name is', 'Ilya']
+
